d5d3fd85f9295de4709cdb123737475115284eb1
lucas caovilla <lucasgrisac@gmail.com>
2025-01-13 22:12:04 -0300

    Creates basic components.
    Creates basic project components, integrates layout component with outlet and integrates routing on app root.

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,18 @@
 import React from 'react';
 import './App.scss';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Layout from './layout/Layout/Layout';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.scss';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>Header Component</h1>
+    </div>
+  );
+};
+
+export default Header;
