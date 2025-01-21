7c651e4fe6e123f18f77d0a9bea0dc268490e72d
lucas caovilla <lucasgrisac@gmail.com>
2025-01-21 18:31:36 -0300

    Adjusts Footer component.
    Adjusts footer component structure and styles regarding the design.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,9 +3,78 @@ import './Footer.scss';
 
 const Footer = () => {
   return (
-    <div className="footer">
-      <h1>Footer Component</h1>
-    </div>
+    <footer className="footer">
+      <figure className="logo">
+        <img src="/static/images/Logo.svg" alt="Logo Bebecê" />
+      </figure>
+      <ul className="social-medias">
+        <li className="social-media">
+          <a>
+            <img src="/static/images/social-medias/instagram.svg" alt="Instagram"></img>
+          </a>
+        </li>
+        <li className="social-media">
+          <a>
+            <img src="/static/images/social-medias/facebook.svg" alt="Facebook"></img>
+          </a>
+        </li>
+        <li className="social-media">
+          <a>
+            <img src="/static/images/social-medias/pinterest.svg" alt="Pinterest"></img>
+          </a>
+        </li>
+        <li className="social-media">
+          <a>
+            <img src="/static/images/social-medias/twitter.svg" alt="Twitter"></img>
+          </a>
+        </li>
+        <li className="social-media">
+          <a>
+            <img src="/static/images/social-medias/tik-tok.svg" alt="TikTok"></img>
+          </a>
+        </li>
+      </ul>
+      <figure className="about-us">
+        <figcaption>Sobre a empresa</figcaption>
+        <ul>
+          <li>
+            <a>
+              Quem somos
+            </a>
+          </li>
+          <li>
+            <a>
+              Fale conosco
+            </a>
+          </li>
+        </ul>
+      </figure>
+      <figure className="policies">
+        <figcaption>Sobre a empresa</figcaption>
+        <ul>
+          <li>
+            <a>
+              Política de Privacidade
+            </a>
+          </li>
+          <li>
+            <a>
+              Termos de Uso
+            </a>
+          </li>
+          <li>
+            <a>
+              Política de Entrega
+            </a>
+          </li>
+          <li>
+            <a>
+              Política de Cupom e Descontos
+            </a>
+          </li>
+        </ul>
+      </figure>
+    </footer>
   );
 };
 
